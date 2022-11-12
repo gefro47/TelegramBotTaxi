dea65ecb3101985a2b6f55d590eec0d2ab26a1b1
Bogdan Markov <bogdick@yandex.ru>
2022-11-12 13:11:00 +0100

    DriverBot.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 import dev.inmo.tgbotapi.extensions.api.bot.getMe
-import dev.inmo.tgbotapi.extensions.api.send.reply
 import dev.inmo.tgbotapi.extensions.behaviour_builder.telegramBotWithBehaviourAndLongPolling
-import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
+import driverlogic.driverBot
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
@@ -13,8 +12,7 @@ suspend fun main(args: Array<String>) {
 //        Todo logic
     }
     telegramBotWithBehaviourAndLongPolling(Hack_Taxi_Drivers_Bot, CoroutineScope(Dispatchers.Default)) {
-        println(getMe())
-//        Todo logic
+        driverBot(this)
     }.second.join()
 
 }
