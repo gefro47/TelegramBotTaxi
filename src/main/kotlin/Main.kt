8ed24beccf43c18dbeb4a1bf74b66a2d7a90c8d2
gefro <gefro47@gmail.com>
2022-11-12 12:11:15 +0100

    1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,19 @@
-import kotlinx.coroutines.GlobalScope
+import dev.inmo.tgbotapi.bot.ktor.telegramBot
+import dev.inmo.tgbotapi.extensions.api.bot.getMe
+import dev.inmo.tgbotapi.extensions.api.send.reply
+import dev.inmo.tgbotapi.extensions.behaviour_builder.buildBehaviourWithLongPolling
+import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onCommand
 
-fun main(args: Array<String>) {
 
+suspend fun main(args: Array<String>) {
 
+    val bot = telegramBot(Hack_Taxi_Client_Bot)
 
+    bot.buildBehaviourWithLongPolling {
+        println(getMe())
+
+        onCommand("start") {
+            reply(it, "Hi:)")
+        }
+    }.join()
 }
